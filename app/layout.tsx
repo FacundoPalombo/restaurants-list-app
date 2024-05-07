b194ededa551338fd5b5820ce2260cb6356eada8
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2024-05-07 20:04:33 +0200

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             Prueba técnica ©Tailor hub SL 2019 - 2024
           </p>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
